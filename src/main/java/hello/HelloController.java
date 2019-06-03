0994726f5c4dbb0cb73019dcc3808b3dac1d3998
OmerCeliker <OmerCeliker@users.noreply.github.com>
2019-06-03 17:04:52 -0400

    Update HelloController.java

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot v10!";
+        return "Greetings from Spring Boot v11!";
     }
     
 }
